83f8d1abee1f54c514186c1e0a5aec3e13db9bb7
Daniel Xu <xudh47@gmail.com>
2022-09-25 16:47:29 -0700

    Updated functions

--- index.js
+++ index.js
@@ -13,22 +13,68 @@ function startApp() {
         name: "choice",
         message: "What would you like to do?",
         choices: [
-          "View all departments",
-          "View all roles",
-          "View all employees",
-          "Add a department",
-          "Add a role",
-          "Add an employee",
-          "Update an employee role",
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
           "Quit",
         ],
       },
     ])
     .then((options) => {
       switch (options.choice) {
-        case "View all departments":
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+
+        case "Add Employee":
+          addEmployees();
+          break;
+
+        case "Update Employee Role":
+          updateEmployee();
+          break;
+
+        case "View All Roles":
+          viewRoles();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "View All Departments":
           viewDept();
           break;
+
+        case "Add Department":
+          addDepartment();
+          break;
+
+        default:
+          process.exit();
       }
     });
 }
+
+function viewAllEmployees() {}
+
+function addEmployees() {}
+
+function updateEmployee() {}
+
+function viewRoles() {}
+
+function addRole() {}
+
+function viewDept() {
+  db.query("SELECT * FROM department").then((result, err) => {
+    if (err) console.err(err);
+    console.table(result);
+    startApp();
+  });
+}
+function addDepartment() {}
