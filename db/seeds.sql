4b00a382785b4a1ae8b8d029a0c603e2c4dca393
Daniel Xu <xudh47@gmail.com>
2022-09-25 16:02:31 -0700

    Updated db and index.js

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (name)
+VALUES
+('Engineering'),
+('Finance'),
+('Legal'),
+('Sales');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Lead', 100000, 4),
+('Salesperson', 80000, 4),
+('Lead Engineer', 150000, 1),
+('Software Engineer', 120000, 1),
+('Account Manager', 160000, 2),
+('Accountant', 125000, 2)
+('Legal Team Lead', 250000, 3),
+('Lawyer', 190000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Ludwig', 'Beethoven', 1, null),
+('Johann', 'Bach', 2, null),
+('Wolfgang', 'Mozart', 3, null),
+('Johannes', 'Brahms', 4, null),
+('Richard', 'Wagner', 5, 1),
+('Claude', 'Debussy', 6, null),
+('Pyotr', 'Tchaikovsky', 7, null),
+('Frederic', 'Chopin', 8, null);
--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-// Index.js initiates the app and prompts the questions
-
-const { prompt } = require("inquirer");
-const logo = require("asciliart-logo");
-const db = require("./db");
+const inquirer = require("inquirer");
+const db = require("./db/connection");
 require("console.table");
 const utils = require("util");
 db.query = utils.promisify(db.query);
+const logo = require("asciliart-logo");
+
+function startApp() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Quit",
+        ],
+      },
+    ])
+    .then((options) => {
+      switch (options.choice) {
+        case "View all departments":
+          viewDept();
+          break;
+      }
+    });
+}
